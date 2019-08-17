0a5518985578f48cfe54d76bd12881545912a678
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-08-17 15:11:33 +0530

    Update lazy_af.sh

--- lazy_af.sh
+++ lazy_af.sh
@@ -5,3 +5,7 @@ rm -rf kernel/asus/sdm660
 git clone https://github.com/EndLess728/Etherious-EAS.git -b Etherious-HMP kernel/asus/sdm660
 rm -rf vendor/asus/X01BD
 git clone https://github.com/EndLess728/android_vendor_asus_X01BD.git -b 9.0 vendor/asus/X01BD
+
+
+#Remove conflicts (Already defined in device tree) 
+rm -rf vendor/cos/packages/overlays/NoCutoutOverlay
