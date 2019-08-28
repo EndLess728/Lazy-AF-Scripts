2e178b48d3f783cb70a58339ed8e2ab11e2f64de
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-08-28 09:24:57 +0530

    Update lazy_af.sh

--- lazy_af.sh
+++ lazy_af.sh
@@ -1,11 +1,11 @@
 #Device specific BLOBS 
 rm -rf device/asus/X01BD
-git clone https://github.com/EndLess728/device_asus_X01BD.git -b cosmic device/asus/X01BD
+git clone https://github.com/asusmaxprom2/android_device_asus_X01BD.git -b test device/asus/X01BD
 rm -rf kernel/asus/sdm660
-git clone https://github.com/EndLess728/Etherious-EAS.git -b Etherious-HMP kernel/asus/sdm660
+git clone https://github.com/asusmaxprom2/android_kernel_asus_X01BD -b arrow-9.x kernel/asus/sdm660
 rm -rf vendor/asus/X01BD
-git clone https://github.com/EndLess728/android_vendor_asus_X01BD.git -b 9.0 vendor/asus/X01BD
+git clone https://github.com/asusmaxprom2/android_vendor_asus_X01BD vendor/asus/X01BD
 
 
 #Remove conflicts (Already defined in device tree) 
-rm -rf vendor/cos/packages/overlays/NoCutoutOverlay
+#rm -rf vendor/cos/packages/overlays/NoCutoutOverlay
