23d87968ce95beef9c97cbfe9500af32b8fcf276
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-09-02 14:41:11 +0530

    Update lazy_af.sh

--- lazy_af.sh
+++ lazy_af.sh
@@ -1,6 +1,6 @@
 #Device specific BLOBS 
 rm -rf device/asus/X01BD
-git clone https://github.com/asusmaxprom2/android_device_asus_X01BD.git -b test device/asus/X01BD
+git clone https://github.com/asusmaxprom2/android_device_asus_X01BD.git -b beast device/asus/X01BD
 rm -rf kernel/asus/sdm660
 git clone https://github.com/asusmaxprom2/android_kernel_asus_X01BD -b arrow-9.x kernel/asus/sdm660
 rm -rf vendor/asus/X01BD
