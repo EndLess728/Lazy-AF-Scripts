3b2bd5654d73d6245385b975fa453d69e7994292
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-09-03 18:38:59 +0530

    Update build_cosmic.sh

--- build_cosmic.sh
+++ build_cosmic.sh
@@ -6,6 +6,13 @@ git clone https://github.com/asusmaxprom2/android_kernel_asus_X01BD -b arrow-9.x
 rm -rf vendor/asus/X01BD
 git clone https://github.com/asusmaxprom2/android_vendor_asus_X01BD vendor/asus/X01BD
 
+## My Cosmic Stuff
+rm -rf fram*/base
+git clone https://github.com/asusmaxprom2/platform_frameworks_base -b corona-release frameworks/base
+rm -rf packages/apps/Longshot
+git clone https://github.com/PixelExperience/packages_apps_Longshot packages/apps/Longshot
+rm -rf vendor/cos
+git clone https://github.com/asusmaxprom2/platform_vendor_cos.git -b corona-release vendor/cos
 
 #Remove conflicts (Already defined in device tree) 
 rm -rf device/asus/X01BD/data-ipa-cfg-mgr
