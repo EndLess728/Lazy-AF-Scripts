2500b60046233a67591ca30f547366f92b5601ee
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-12-15 21:35:42 +0530

    Update durex-clang.sh

--- durex-clang.sh
+++ durex-clang.sh
@@ -7,18 +7,19 @@ rm -rf vendor/asus/X01BD
 git clone https://github.com/X01BD-Android-Q/android_vendor_asus_X01BD.git -b vendor/asus/X01BD
 
 #Track Dirac
-rm -rf packages/apps/Dirac
-git clone https://github.com/asusmaxprom2/android_packages_apps_Dirac.git packages/apps/Dirac
+#rm -rf packages/apps/Dirac
+#git clone https://github.com/asusmaxprom2/android_packages_apps_Dirac.git packages/apps/Dirac
 
 #Track Clang from Havoc
 rm -rf prebuilts/clang/host/linux-x86
-git clone https://github.com/Havoc-OS/android_prebuilts_clang_host_linux-x86 prebuilts/clang/host/linux-x86
+git clone https://github.com/SonalSingh18/prebuilts_clang_host_linux-x86_clang-10.0.1.git prebuilts/clang/host/linux-x86/clang-10.0.1
 
 #Remove conflicts (Already defined in device tree) 
-rm -rf device/asus/X01BD/data-ipa-cfg-mgr
+#rm -rf device/asus/X01BD/data-ipa-cfg-mgr
 #rm -rf vendor/cos/packages/overlays/NoCutoutOverlay
 
 
 ## Build commands
 . build/envsetup.sh
-brunch du_X01BD-userdebug
+lunch du_X01BD-userdebug
+brunch X01BD
