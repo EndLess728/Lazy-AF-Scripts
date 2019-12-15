2bcc962a607e051611ab4e574ed3923071f1765f
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-12-15 21:33:47 +0530

    Update durex-clang.sh

--- durex-clang.sh
+++ durex-clang.sh
@@ -1,10 +1,10 @@
 #Device specific BLOBS 
 rm -rf device/asus/X01BD
-git clone https://github.com/asusmaxprom2/android_device_asus_X01BD.git -b durex-clang device/asus/X01BD
+git clone https://github.com/Flutter-Noob/android_device_asus_X01BD.git -b du device/asus/X01BD
 rm -rf kernel/asus/sdm660
-git clone https://github.com/wahyu17/Aegis-X01BD.git -b Aegis-Pie kernel/asus/sdm660
+git clone https://github.com/SonalSingh18/android_kernel_asus_sdm660.git -b ten kernel/asus/sdm660
 rm -rf vendor/asus/X01BD
-git clone https://github.com/asusmaxprom2/android_vendor_asus_X01BD vendor/asus/X01BD
+git clone https://github.com/X01BD-Android-Q/android_vendor_asus_X01BD.git -b vendor/asus/X01BD
 
 #Track Dirac
 rm -rf packages/apps/Dirac
