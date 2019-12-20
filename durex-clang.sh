9cc84c142ddbd3b8a9c6fb6a25f6b206ef039d14
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-12-20 22:49:36 +0530

    Update durex-clang.sh

--- durex-clang.sh
+++ durex-clang.sh
@@ -17,6 +17,9 @@ git clone https://github.com/SonalSingh18/prebuilts_clang_host_linux-x86_clang-1
 rm -rf packages/apps/Snap
 git clone https://github.com/Havoc-OS/android_packages_apps_Snap.git -b ten packages/apps/Snap
 
+rm -rf packages/apps/Messaging
+git clone https://github.com/LineageOS/android_packages_apps_Messaging.git packages/apps/Messeging
+
 #Remove conflicts (Already defined in device tree) 
 #rm -rf device/asus/X01BD/data-ipa-cfg-mgr
 #rm -rf vendor/cos/packages/overlays/NoCutoutOverlay
