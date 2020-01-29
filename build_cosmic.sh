bad103e59b4c4705171b3aed80eb0c37e8087079
Mr. EndLess <aryanmk.mk5@gmail.com>
2020-01-29 11:15:42 +0530

    temp changes

--- build_cosmic.sh
+++ build_cosmic.sh
@@ -10,10 +10,6 @@ git clone https://github.com/asusmaxprom2/proprietary_vendor_asus_X01BD.git -b l
 #rm -rf prebuilts/clang/host/linux-x86
 #git clone https://github.com/SonalSingh18/prebuilts_clang_host_linux-x86_clang-10.0.1.git prebuilts/clang/host/linux-x86/clang-10.0.1
 
-#Device Settings for Doze
-#rm -rf packages/resources/devicesettings
-#git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git packages/resources/devicesettings
-
 #hals
 rm -rf hardware/qcom/media
 #git clone https://github.com/SonalSingh18/Q-Hals.git -b media-pa hardware/qcom/media/msm8998
@@ -25,22 +21,15 @@ rm -rf hardware/qcom/display
 git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-17.0-caf-msm8998 hardware/qcom/display/msm8998
 #rm -rf hardware/qcom/wlan/msm8998
 #git clone https://github.com/SonalSingh18/Q-Hals.git -b wlan-caf hardware/qcom/wlan/msm8998
-## My Cosmic Stuff
-#rm -rf fram*/base
-#git clone https://github.com/asusmaxprom2/platform_frameworks_base -b corona-release frameworks/base
-#rm -rf packages/apps/Longshot
-#git clone https://github.com/PixelExperience/packages_apps_Longshot packages/apps/Longshot
-#rm -rf vendor/cos
-#git clone https://github.com/asusmaxprom2/platform_vendor_cos.git -b corona-release vendor/cos
 
 #Remove conflicts (Already defined in device tree) 
-#rm -rf device/asus/X01BD/data-ipa-cfg-mgr
+rm -rf device/asus/X01BD/data-ipa-cfg-mgr
 rm -rf vendor/cos/packages/overlays/NoCutoutOverlay
-rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
+#rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 #Add Wlan Hals from AOSPA
-rm -rf hardware/qcom/wlan
-git clone https://github.com/AOSPA/android_hardware_qcom_wlan.git hardware/qcom/wlan
+#rm -rf hardware/qcom/wlan
+#git clone https://github.com/AOSPA/android_hardware_qcom_wlan.git hardware/qcom/wlan
 
 #Wireless ANT 
 https://github.com/LineageOS/android_external_ant-wireless_antradio-library.git external/ant-wireless/antradio-library
