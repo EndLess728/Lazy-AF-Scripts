5111b383f09319a3731cd763499aa7414ee946e8
Mr. EndLess <aryanmk.mk5@gmail.com>
2020-03-19 11:51:37 +0530

    update to corvus

--- durex-clang.sh
+++ durex-clang.sh
@@ -1,10 +1,10 @@
 #Device specific BLOBS 
 rm -rf device/asus/X01BD
-git clone https://github.com/Flutter-Noob/android_device_asus_X01BD.git -b du device/asus/X01BD
+git clone https://github.com/EndLess728/android_device_asus_X01BD.git -b corvus device/asus/X01BD
 rm -rf kernel/asus/sdm660
 git clone https://github.com/SonalSingh18/android_kernel_asus_sdm660.git -b ten kernel/asus/sdm660
 rm -rf vendor/asus/X01BD
-git clone https://github.com/X01BD-Android-Q/android_vendor_asus_X01BD.git -b q vendor/asus/X01BD
+git clone https://github.com/EndLess728/android_vendor_asus_X01BD.git -b ten vendor/asus/X01BD
 
 #Track Dirac
 #rm -rf packages/apps/Dirac
@@ -12,7 +12,7 @@ git clone https://github.com/X01BD-Android-Q/android_vendor_asus_X01BD.git -b q 
 
 #Track Clang from Havoc
 rm -rf prebuilts/clang/host/linux-x86
-git clone https://github.com/SonalSingh18/prebuilts_clang_host_linux-x86_clang-10.0.1.git prebuilts/clang/host/linux-x86/clang-10.0.1
+git clone https://github.com/SonalSingh18/prebuilts_clang_host_linux-x86_clang-10.0.4.git prebuilts/clang/host/linux-x86/clang-10.0.4
 
 #rm -rf packages/apps/Snap
 #git clone https://github.com/Havoc-OS/android_packages_apps_Snap.git -b ten packages/apps/Snap
